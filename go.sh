1b10bd5f0130643d446469f39d7873458ad7649f
YenTen dot Me <support@yenten.me>
2020-07-03 01:20:25 +0000

    tested

--- go.sh
+++ go.sh
@@ -2,9 +2,13 @@
 #This will activate background processing on iOS using geolocation:
 cat /dev/location > /dev/null &
 ./yescompute 8989 &
+sleep 2
 ./yescompute 8990 &
 sleep 5
 echo  "GO"
 ./ni.sh &
+sleep 5
 ./ni2.sh &
+
+echo "MINING!"
 
